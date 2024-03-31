c2ed37eddf6560cddcc9c46f9e67acfa228e72fb
lina_gd <clgdiaz.2@gmail.com>
2024-03-31 01:04:49 -0500

    react done

--- src/NameTag.js
+++ src/NameTag.js
@@ -9,13 +9,13 @@ The JSX returned should consist of:
     - a h2 element with className attribute of "name" and the inner text of your name
 */
 const NameTag = () => (
-  <div>
-    <h3 className = "title">HELLO</h3>
-    <p className = "subtitle">my name is</p>
-    <h2 className = "name">Liria</h2>
+  <div className="name-tag">
+    <h3 className="title">HELLO</h3>
+    <p className="subtitle">my name is</p>
+    <h2 className="name">Jesse</h2>
   </div>
 );
+
 /* export our functional component definition to be 
 imported then rendered within another module */
 export default NameTag;
-
